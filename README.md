f7b5f9dace7b3b2dabd241f4bd446f98f1dba239
Paula Soto <psortiz2004@gmail.com>
2025-04-23 15:54:09 +0200

    JS + JQ y intento llamada 1 a API

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AnimalCrossing-Wishlist
  
+- API KEY: 2cb526da-fa3e-4a24-b550-6aad92eb0c02
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log("ready!");
+    fetch("https://api.nookipedia.com/villagers", {
+        method: "GET",
+        headers: {"X-API-KEY": "2cb526da-fa3e-4a24-b550-6aad92eb0c02"}
+
+    })
+    .then;
+
+
+});
+
+

c69b42991334701638c0ea2e63e2586d8ef537c8
mimieow <angy2004lol@hotmail.com>
2025-05-20 21:13:14 +0200

    Formulario entero

--- js/script.js
+++ js/script.js
@@ -97,3 +97,43 @@ $(document).ready(function () {
         console.error("Error al obtener datos:", error);
     });
 });
+
+  // VALIDACIÓN DE FORMULARIO
+  const form = document.querySelector('.needs-validation');
+
+  if (form) {
+      form.addEventListener('submit', function (event) {
+          const passwordInput = document.getElementById('password');
+          const emailInput = document.getElementById('email');
+          const password = passwordInput.value;
+          const email = emailInput.value;
+
+          const specialCharRegex = /[^A-Za-z0-9]/;
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+          let valid = true;
+
+          // Validar email
+          if (!emailRegex.test(email)) {
+              emailInput.setCustomValidity("Introduce un email válido.");
+              valid = false;
+          } else {
+              emailInput.setCustomValidity("");
+          }
+
+          // Validar contraseña
+          if (!specialCharRegex.test(password)) {
+              passwordInput.setCustomValidity("La contraseña debe contener al menos un carácter especial.");
+              valid = false;
+          } else {
+              passwordInput.setCustomValidity("");
+          }
+
+          if (!form.checkValidity() || !valid) {
+              event.preventDefault();
+              event.stopPropagation();
+          }
+
+          form.classList.add('was-validated');
+      }, false);
+  }
